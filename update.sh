03c0ee72590d0ddd20ed7b7811b5a20f57a7bbfe
KicamonIce <1532931259@qq.com>
2023-05-21 00:40:48 +0800

    changeshell

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 cp -rf ~/.config/alacritty ~/.config/kitty ~/.config/nvim ~/.config/ranger ~/.config/dunst ~/.config/i3 ~/.config/neofetch ~/.config/picom ~/.config/zsh ./
 git add .
-git commit -m '$1'
+git commit -m $1
 git push
