d72909f0a9e1f9a606c82a9046763c390cc04c13
KicamonIce <1532931259@qq.com>
2023-05-22 00:16:29 +0800

    add_some

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-cp -rf ~/.config/alacritty ~/.config/kitty ~/.config/nvim ~/.config/ranger ~/.config/dunst ~/.config/i3 ~/.config/neofetch ~/.config/picom ~/.config/zsh ./
+cp -rf ~/.config/alacritty ~/.config/kitty ~/.config/ranger ~/.config/dunst ~/.config/i3 ~/.config/neofetch ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ./
 git add .
 git commit -m $1
 git push
