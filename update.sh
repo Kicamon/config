fa4bfce1140e010b402e98562cca3479cceacc21
KicamonIce <1532931259@qq.com>
2023-06-16 23:52:04 +0800

    mdp

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-cp -rf ~/.config/alacritty ~/.config/kitty ~/.config/ranger ~/.config/dunst ~/.config/i3 ~/.config/neofetch ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/fish ~/.config/starship.toml ./
+cp -rf ~/.config/alacritty ~/.config/kitty ~/.config/ranger ~/.config/dunst ~/.config/i3 ~/.config/neofetch ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/wallpaper ~/.config/zsh ~/.config/fish ~/.config/starship.toml ~/.config/mpd ./
 git add .
 git commit -m $1
 git push
